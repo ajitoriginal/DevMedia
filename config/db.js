967f8210795ff32c44c62281796863e137a3910b
Ajit Singh <86841688+ajit-original@users.noreply.github.com>
2022-05-25 08:29:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,17 +3,13 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        });
-        console.log('MongoDB Connected...');
-    }
-    catch(err){
-        console.error(err.message);
-        //Exit process with failure
-        process.exit (1);
-    }
-}
+  try {
+    await mongoose.connect(db);
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
