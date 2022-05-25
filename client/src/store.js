331f83639f6c675934143c06d75ae02afe6e76e3
Ajit Singh <86841688+ajit-original@users.noreply.github.com>
2022-05-25 08:54:33 +0530

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,31 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
+import { applyMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import setAuthToken from './utils/setAuthToken';
 
-const initialState = {}
+const initialState = {};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-)
+const store = configureStore(
+  { reducer: rootReducer },
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
+let currentState = store.getState();
+
+store.subscribe(() => {
+  // keep track of the previous and current state to compare changes
+  let previousState = currentState;
+  currentState = store.getState();
+  // if the token changes set the value in localStorage and axios headers
+  if (previousState.auth.token !== currentState.auth.token) {
+    const token = currentState.auth.token;
+    setAuthToken(token);
+  }
+});
+
+export default store;
