798d2ccd37ec5ac43f953c2aefb26dfc7530f761
Ajit Singh <86841688+ajit-original@users.noreply.github.com>
2022-05-25 08:31:13 +0530

    Update User.js

--- Models/User.js
+++ Models/User.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose'); // importing mongoose
+const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({ // defining user model
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    requried: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = User = mongoose.model('user', UserSchema); // exporting User Schema module
+module.exports = User = mongoose.model('user', UserSchema);
