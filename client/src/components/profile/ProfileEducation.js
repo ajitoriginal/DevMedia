2793da466b9d0f4747eb33cbab66056e2ae26080
Ajit Singh <86841688+ajit-original@users.noreply.github.com>
2022-05-25 08:42:06 +0530

    Update ProfileEducation.js

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Moment from 'react-moment'
+import React from 'react';
+import PropTypes from 'prop-types';
+import formatDate from '../../utils/formatDate';
 
 const ProfileEducation = ({
-    education: { school, degree, fieldofstudy, current, to, from, description }
+  education: { school, degree, fieldofstudy, current, to, from, description },
 }) => (
-    <div>
-        <h3 className="text-dark">{school}</h3>
-        <p>
-            <Moment format='YYYY/MM/DD'>{from}</Moment> -{' '}
-            {!to ? ' Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
-        </p>
-        <p>
-            <strong>Degree: </strong> {degree}
-        </p>
-        <p>
-            <strong>Field Of Study: </strong> {fieldofstudy}
-        </p>
-        <p>
-            <strong>Description: </strong> {description}
-        </p>
-    </div>
-)
+  <div>
+    <h3 className='text-dark'>{school}</h3>
+    <p>
+      {formatDate(from)} - {to ? formatDate(to) : 'Now'}
+    </p>
+    <p>
+      <strong>Degree: </strong> {degree}
+    </p>
+    <p>
+      <strong>Field Of Study: </strong> {fieldofstudy}
+    </p>
+    <p>
+      <strong>Description: </strong> {description}
+    </p>
+  </div>
+);
 
 ProfileEducation.propTypes = {
-    education: PropTypes.array.isRequired
-}
+  education: PropTypes.object.isRequired,
+};
 
-export default ProfileEducation
+export default ProfileEducation;
