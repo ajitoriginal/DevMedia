b1258a2bc7814d1b32eadf44f6de8e45f98f21e4
Ajit Singh <86841688+ajit-original@users.noreply.github.com>
2022-05-25 08:54:05 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,85 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import EditProfile from './components/profile-forms/EditProfile';
+import ProfileForm from './components/profile-forms/ProfileForm';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
-// import Routes from './components/routing/Routes'
-import { loadUser } from './actions/auth'
-import setAuthToken from './utils/setAuthToken'
-import './App.css';
-// // redux
+import NotFound from './components/layout/NotFound';
+import PrivateRoute from './components/routing/PrivateRoute';
+import { LOGOUT } from './actions/types';
 
+// Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token)
-  
-}
-
+import './App.css';
 
 const App = () => {
-    
   useEffect(() => {
-    store.dispatch(loadUser())
-  }, [])
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
   return (
     <Provider store={store}>
-    <Router>
-      <Fragment>
-         <Navbar />  
-         <Route exact path="/" component={Landing} />
-         <section className="container">
-             <Alert />
-             <Switch>
-                 <Route exact path="/register" component={Register} />
-                 <Route exact path="/login" component={Login} />
-                 <Route exact path="/profiles" component={Profiles} />
-                 <Route exact path="/profile/:id" component={Profile} />
-                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                 <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-                 <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-                 <PrivateRoute exact path="/add-experience" component={AddExperience} />
-                 <PrivateRoute exact path="/add-education" component={AddEducation} />
-                 <PrivateRoute exact path="/posts" component={Posts} />
-                 <PrivateRoute exact path="/posts/:id" component={Post} />
-             </Switch>
-         </section>
-      </Fragment>
-     </Router>
-     </Provider>
-  )
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+          <Route path='profiles' element={<Profiles />} />
+          <Route path='profile/:id' element={<Profile />} />
+          <Route
+            path='dashboard'
+            element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path='create-profile'
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path='edit-profile'
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path='add-experience'
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path='add-education'
+            element={<PrivateRoute component={AddEducation} />}
+          />
+          <Route path='posts' element={<PrivateRoute component={Posts} />} />
+          <Route path='posts/:id' element={<PrivateRoute component={Post} />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 };
-
 
 export default App;
